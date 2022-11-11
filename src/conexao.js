04345773e9a0016ff5ef4992e1ed661dc55746ff
FELIPE JESUS ROSARIO <felipejr.2021@outlook.com>
2022-11-11 18:12:17 -0300

    novo database

--- src/conexao.js
+++ src/conexao.js
@@ -2,13 +2,11 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         user: "doadmin",
-        host: "market-cubos-do-user-12862024-0.b.db.ondigitalocean.com",
+        host: "felipe-cubos-marketplace-do-user-12862024-0.b.db.ondigitalocean.com",
         database: "defaultdb",
         password: "AVNS_Ne2rqHEgkZtxvindX1s",
         port: 25060,
-        ssl: {
-            rejectUnathorized: false
-        }
+        sslmode: require
     }
 });
 
