13e7874cb7504dff3dd48e91ed5f259c0270fb27
FELIPE JESUS ROSARIO <felipejr.2021@outlook.com>
2022-11-11 17:53:32 -0300

    ssl

--- src/conexao.js
+++ src/conexao.js
@@ -5,6 +5,10 @@ const knex = require('knex')({
         host: "market-cubos-do-user-12862024-0.b.db.ondigitalocean.com",
         database: "defaultdb",
         password: "AVNS_Ne2rqHEgkZtxvindX1s",
+        port: 25060,
+        ssl: {
+            rejectUnathorized: false
+        }
     }
 });
 
